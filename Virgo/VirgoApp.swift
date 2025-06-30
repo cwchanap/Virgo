f40b2904a3bbd16abf4af600e0cb3365848085a7
cwchanap <cwchanap@connect.ust.hk>
2025-06-29 17:54:37 -0700

    Initial Commit

---
+++ Virgo/VirgoApp.swift
@@ -0,0 +1,32 @@
+//
+//  VirgoApp.swift
+//  Virgo
+//
+//  Created by Chan Wai Chan on 29/6/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VirgoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ VirgoTests/VirgoTests.swift
@@ -0,0 +1,16 @@
+//
+//  VirgoTests.swift
+//  VirgoTests
+//
+//  Created by Chan Wai Chan on 29/6/2025.
+//
+
+import Testing
+
+struct VirgoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
