1b5357c515933a27fa7d3f5609e31429b86cd3a0
cwchanap <cwchanap@connect.ust.hk>
2026-02-07 21:44:44 -0800

    fix: add app.terminate() to UI test teardown to prevent state leakage

--- VirgoUITests/GameplayViewUITests.swift
+++ VirgoUITests/GameplayViewUITests.swift
@@ -22,7 +22,7 @@ final class GameplayViewUITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app.terminate()
     }
 
     @MainActor
