5a8f0b17cd9bbfabbdb24345e016c0701ba86164
cwchanap <cwchanap@connect.ust.hk>
2026-09-11 10:33:26 -0700

    feat: render native DrumNotation primitives

--- Packages/DrumNotation/Tests/DrumNotationTests/PackageBoundaryTests.swift
+++ Packages/DrumNotation/Tests/DrumNotationTests/PackageBoundaryTests.swift
@@ -5,3 +5,19 @@ import DrumNotation
 func packageImports() {
     #expect(NotationDuration.quarter.rawValue == "quarter")
 }
+
+@Test("public primitive views construct from semantic values")
+func primitiveViewsConstruct() {
+    _ = PercussionNoteheadView(
+        style: .normal,
+        duration: .quarter,
+        staffSpace: 20
+    )
+    _ = NotationRestGlyphView(duration: .quarter, staffSpace: 20)
+    _ = NotationFlagGlyphView(
+        duration: .sixteenth,
+        direction: .up,
+        staffSpace: 20
+    )
+    _ = PercussionArticulationView(articulation: .open, staffSpace: 20)
+}
